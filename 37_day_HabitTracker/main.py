647852a1c25b845316ca05e92df4c6f574178480
BayramWilson <bayram.wilson04@gmail.com>
2025-05-10 12:22:22 +0200

    create pixels now

--- 37_day_HabitTracker/main.py
+++ 37_day_HabitTracker/main.py
@@ -1,11 +1,16 @@
 import requests
 import os
 from dotenv import load_dotenv
+import datetime
 load_dotenv()
 
 USERNAME = os.getenv("PIXELA_USERNAME")
 TOKEN = os.getenv("PIXELA_TOKEN")
 
+current_date = datetime.datetime.now()
+print(current_date)
+
+pixela_url = "https://pixe.la"
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
     "token": TOKEN,
@@ -33,6 +38,15 @@ response_cre = requests.post(url=graph_endpoint, json=create_graph, headers=head
 
 # print(response_cre.text)
 
-pixela_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{create_graph["id"]}"
+cre_px_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{create_graph["id"]}"
 
-create_pixel
+create_pixel = {
+    "date": current_date.strftime("%Y%m%d"),
+    "quantity": "2",
+}
+
+create_pixel = requests.post(url= cre_px_endpoint, json=create_pixel, headers=headers )
+
+get_user_link = f"{pixela_url}/@{USERNAME}"
+print(get_user_link)
+print(create_pixel.text)
