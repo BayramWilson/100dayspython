c2c0b02b80a2e03cc065528ffb794b7fe289f736
BayramWilson <bayram.wilson04@gmail.com>
2025-05-12 14:59:49 +0200

    added funtion to retrieve sheets info, a fn to manipulate sheet info

--- 39_day_cheapFlightFinder/data_manager.py
+++ 39_day_cheapFlightFinder/data_manager.py
@@ -1,3 +1,16 @@
+from dotenv import load_dotenv
+import os
+import requests
+load_dotenv()
+
+sheety_api_key = os.getenv("SHEETY_AUTH_HEADER")
+headers = {
+    "Authorization": sheety_api_key
+}
+request_body = {
+        
+}
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self,city,iata,lowestPrice):
@@ -5,6 +18,24 @@ class DataManager:
         self.age = iata
         self.lowestPrice = lowestPrice  
 
-    def sheetInfo():
-        pass
+def getSheetInfo():
+    """This function retrieves the info of Google Sheet"""
+    
+    retrieve_rows_ep = "https://api.sheety.co/ffc8b9a99ae35d142dc788b0c5a5e26e/flightDeals/prices"
+    response = requests.get(url=retrieve_rows_ep, headers=headers)
+    return response.text
+
+    
+def mutSheetInfo(objID):
+     """This function can manipulate the sheet, required is a objectID"""
+     
+     put_body = {
+         "price": {
+             "IATA Code": "TEST"
+         }
+     }
+     mut_rows_ep = f"https://api.sheety.co/ffc8b9a99ae35d142dc788b0c5a5e26e/flightDeals/prices/{objID}"
+     response = requests.put(url=mut_rows_ep, json=put_body, headers=headers)
+     return response.text
+
     
--- 39_day_cheapFlightFinder/main.py
+++ 39_day_cheapFlightFinder/main.py
@@ -1,11 +1,9 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
+from data_manager import *
 from pprint import pprint
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+sheet_data = getSheetInfo()
+# print(sheet_data)
 
-sheety_api_key = os.getenv("SHEETY_AUTH_HEADER")
-
-pprint(sheety_api_key)
+print(mutSheetInfo(2))
