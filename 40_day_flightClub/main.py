001f45b919bd7c6b5f4f38cc1fc69abee8116fe5
BayramWilson <bayram.wilson04@gmail.com>
2025-05-13 10:51:57 +0200

    finished day 40

--- 40_day_flightClub/main.py
+++ 40_day_flightClub/main.py
@@ -27,13 +27,20 @@ print(f"sheet_data:\n {sheet_data}")
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
 
-# ==================== Search for Flights and Send Notifications ====================
+# ==================== Retrieve your customer emails ====================
+
+customer_data = data_manager.get_customer_emails()
+# Verify the name of your email column in your sheet. Yours may be different from mine
+customer_email_list = [row["whatIsYourEmail?"] for row in customer_data]
+# print(f"Your email list includes {customer_email_list}")
+
+# ==================== Search for direct flights  ====================
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
 for destination in sheet_data:
-    print(f"Getting flights for {destination['city']}...")
+    print(f"Getting direct flights for {destination['city']}...")
     flights = flight_search.check_flights(
         ORIGIN_CITY_IATA,
         destination["iataCode"],
@@ -44,6 +51,22 @@ for destination in sheet_data:
     print(f"{destination['city']}: £{cheapest_flight.price}")
     # Slowing down requests to avoid rate limit
     time.sleep(2)
+
+    # ==================== Search for indirect flight if N/A ====================
+
+    if cheapest_flight.price == "N/A":
+        print(f"No direct flight to {destination['city']}. Looking for indirect flights...")
+        stopover_flights = flight_search.check_flights(
+            ORIGIN_CITY_IATA,
+            destination["iataCode"],
+            from_time=tomorrow,
+            to_time=six_month_from_today,
+            is_direct=False
+        )
+        cheapest_flight = find_cheapest_flight(stopover_flights)
+        print(f"Cheapest indirect flight price is: £{cheapest_flight.price}")
+
+    # ==================== Send Notifications and Emails  ====================
 
     if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
         print(f"Lower price flight found to {destination['city']}!")
