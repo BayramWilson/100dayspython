35327c2cbcbbc4cf328d6f5383317a94421d8e0d
BayramWilson <bayram.wilson04@gmail.com>
2025-05-13 10:22:38 +0200

    Initial Commit day40

--- 39_day_cheapFlightFinder/flight_data.py
+++ 39_day_cheapFlightFinder/flight_data.py
@@ -1,3 +1,33 @@
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+
+    def __init__(self, price, origin_airport, destination_airport, out_date, return_date):
+        """
+        Constructor for initializing a new flight data instance with specific travel details.
+        Parameters:
+        - price: The cost of the flight.
+        - origin_airport: The IATA code for the flight's origin airport.
+        - destination_airport: The IATA code for the flight's destination airport.
+        - out_date: The departure date for the flight.
+        - return_date: The return date for the flight.
+        """
+        self.price = price
+        self.origin_airport = origin_airport
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
+
+def find_cheapest_flight(flights):
+    """
+    Finds the cheapest flight from a list of flight offers.
+    Parameters:
+        flights (list): A list of flight offers returned by the Amadeus API.
+    Returns:
+        dict: The flight offer with the lowest price, or None if no flights are available.
+    """
+    if not flights:
+        print("No flight data available.")
+        return None
+
+    # Find the flight with the lowest price
+    cheapest_flight = min(flights, key=lambda flight: float(flight['price']['grandTotal']))
+    return cheapest_flight
--- 40_day_flightClub/main.py
+++ 40_day_flightClub/main.py
@@ -27,12 +27,13 @@ print(f"sheet_data:\n {sheet_data}")
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
 
-# ==================== Search for Flights ====================
+# ==================== Search for Flights and Send Notifications ====================
+
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
 for destination in sheet_data:
-    print(f"Getting flights for {destination}")
+    print(f"Getting flights for {destination['city']}...")
     flights = flight_search.check_flights(
         ORIGIN_CITY_IATA,
         destination["iataCode"],
@@ -40,8 +41,22 @@ for destination in sheet_data:
         to_time=six_month_from_today
     )
     cheapest_flight = find_cheapest_flight(flights)
-    if cheapest_flight:
-        print(f"Cheapest flight found: £{cheapest_flight['price']['grandTotal']}")
-        if float(cheapest_flight['price']['grandTotal']) < destination["lowestPrice"]:
-            destination["lowestPrice"] = float(cheapest_flight['price']['grandTotal'])
-            data_manager.update_destination_codes()
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
+
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        # notification_manager.send_sms(
+        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        # )
+        # SMS not working? Try whatsapp instead.
+        notification_manager.send_whatsapp(
+            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )
+
+
