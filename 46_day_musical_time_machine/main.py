10cbb0eeb4a0dca3bb92d657fd26b3cde5aeaee3
BayramWilson <bayram.wilson04@gmail.com>
2025-05-19 10:39:49 +0200

    added headers from what is my browser.com

--- 46_day_musical_time_machine/main.py
+++ 46_day_musical_time_machine/main.py
@@ -1,1 +1,25 @@
+URL = "https://www.billboard.com"
 user_input = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
+headers = {
+    "CONTENT-TYPE":"",	
+    "CONTENT-LENGTH":"",
+    "HOST":"www.whatismybrowser.com",
+    "SEC-CH-UA":'"Chromium";v="136", "Brave";v="136", "Not.A/Brand";v="99"',
+    "SEC-CH-UA-MOBILE":"?0",
+    "SEC-CH-UA-ARCH":"x86",
+    "SEC-CH-UA-PLATFORM":"Windows",
+    "SEC-CH-UA-PLATFORM-VERSION":"19.0.0",
+    "SEC-CH-UA-MODEL":"",
+    "UPGRADE-INSECURE-REQUESTS":1,
+    "USER-AGENT":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+    "ACCEPT":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "SEC-GPC":1,
+    "ACCEPT-LANGUAGE":"de-DE,de;q=0.7",
+    "SEC-FETCH-SITE":"same-origin",
+    "SEC-FETCH-MODE":"navigate",
+    "SEC-FETCH-USER":"?1",
+    "SEC-FETCH-DEST":"document",
+    "REFERER":"https://www.whatismybrowser.com/",
+    "ACCEPT-ENCODING":"gzip, deflate, br, zstd",
+    "PRIORITY":"u=0, i"
+}
