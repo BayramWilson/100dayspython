8e9c8a0a33c597151bdbd349a96f1eea0f3b4958
BayramWilson <bayram.wilson04@gmail.com>
2025-05-19 08:35:17 +0200

    challenge done

--- 45_day_webscraping_bs/bs4-start/main.py
+++ 45_day_webscraping_bs/bs4-start/main.py
@@ -1,78 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+# Get the Hacker News front page
 response = requests.get("https://news.ycombinator.com/news")
-website = response.text
+soup = BeautifulSoup(response.text, "html.parser")
 
-soup = BeautifulSoup(website, "html.parser")
+# List to store all post data
+posts_data = []
 
+# Find all posts
+posts = soup.find_all("tr", class_="athing")
 
+for post in posts:
+    titleline = post.find("span", class_="titleline")
+    if titleline:
+        a_tag = titleline.find("a")
+        title = a_tag.get_text()
+        link = a_tag.get("href")
 
+        # Get the upvote info from the next row
+        subtext_row = post.find_next_sibling("tr")
+        score_span = subtext_row.find("span", class_="score")
+        if score_span:
+            upvote_text = score_span.get_text()
+            upvotes = int(upvote_text.split()[0])
+        else:
+            upvotes = 0
 
-# print(soup.select(".title a"))
-# anchor_tag = soup.select(".title a")
-# print(soup.getText(anchor_tag))
-# anchor_tag = soup.find_all(name="a", )
-# anchor_text = soup.getText(anchor_tag)
+        posts_data.append((title, link, upvotes))
 
-# Find the first <span> with class "titleline"
-titleline = soup.find('span', class_='titleline')
+# Sort posts by upvotes descending
+sorted_posts = sorted(posts_data, key=lambda x: x[2], reverse=True)
 
-# Find the <a> tag within it and get the text
-a_text = titleline.find('a').get_text()
-
-a_link = titleline.find("a").get("href")
-
-a_upvote = soup.find("span", class_="score")
-a_upvote = a_upvote.getText()
-# <span class="score" id="score_43982777">40 points</span>
-print(a_text)
-print(a_link)
-print(a_upvote)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import lxml
-
-# with open("website.html") as file:
-#     contents = file.read()
-
-
-# soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title.string)
-
-# print(soup.prettify())
-
-# print(soup.a)
-# print(soup.find_all("a"))
-
-# all_anchor_tags = soup.find_all(name="a")
-# print(all_anchor_tags)
-
-# for tag in all_anchor_tags:
-    # print(tag.getText())
-    # print(tag.get("href"))
-
-# heading = soup.find(name="h1", id="name")
-# print(heading)
-
-# section_heading = soup.find(name="h3", class_="heading")
-# print(section_heading.getText())
-# print(section_heading.name)
-# print(section_heading.get("class"))
-
-# company_url = soup.select_one(selector="p a")
-# print(company_url)
-# print(soup.select(".heading"))
-
+# Print sorted results
+for i, (title, link, upvotes) in enumerate(sorted_posts, start=1):
+    print(f"{i}. {title}")
+    print(f"   Link: {link}")
+    print(f"   Upvotes: {upvotes}")
