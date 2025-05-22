3dfa19f12714cfb05e487ec3cf8453e7848801fe
Rammo Dev <bayram.wilson04@gmail.com>
2025-05-22 17:02:05 +0200

    just make sure the browser is correctly setup in wsl

--- 48_day_browsergamebot_w_Selenium/main.py
+++ 48_day_browsergamebot_w_Selenium/main.py
@@ -1,12 +1,15 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-# Path to correct ChromeDriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.binary_location = "/snap/bin/chromium" 
-
-service = Service("/usr/local/bin/chromedriver")
+# options = Options()
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome()
-driver.get("https://www.amazon.com")
+driver.get("https://example.com")
+
+time.sleep(30)
+print(driver.title)
+# driver.quit()
