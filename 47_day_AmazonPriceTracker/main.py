094cc4c3820c2b9b1233c81367cd279f351feba4
BayramWilson <bayram.wilson04@gmail.com>
2025-05-22 09:02:23 +0200

    got the html and initialized bs4

--- 47_day_AmazonPriceTracker/main.py
+++ 47_day_AmazonPriceTracker/main.py
@@ -1,1 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
 TARGETPRICE=99.95
+URL="https://appbrewery.github.io/instant_pot/"
+html_doc = requests.get(URL)
+html_doc = html_doc.text
+soup = BeautifulSoup(html_doc, 'html.parser')
+# print(soup)
