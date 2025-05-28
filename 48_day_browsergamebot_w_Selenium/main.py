205bb4f51eac45a61e2a9b325d9ca90d9b311cd3
Rammo Dev <bayram.wilson04@gmail.com>
2025-05-28 19:35:43 +0200

    fix: update URL in driver.get() and add event extraction logic

--- 48_day_browsergamebot_w_Selenium/main.py
+++ 48_day_browsergamebot_w_Selenium/main.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 # from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.chrome.options import Options
 
@@ -13,10 +14,30 @@ chrome_options.add_experimental_option("detach", True)
 
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://example.com")
+driver.get("https://python.org")
+
+# event_times = []
+# events_list = []
+# event_information={}
+
+event_times = driver.find_elements(By.CSS_SELECTOR, value="div.medium-widget.event-widget.last time")
+events_names = driver.find_elements(By.CSS_SELECTOR, value="div.medium-widget.event-widget.last li a")
+
+for tim in event_times:
+    print(tim.text)
 
 
+# for event in events:
+#     print(event.text)
 
+events = {}
+
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+            "name": events_names[n].text
+    }
+print(events)
 
 # driver.close()
-# driver.quit()
+driver.quit()
