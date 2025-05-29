c0c8e0df4698df59b43195db06b04df9d1e835a9
Rammo Dev <bayram.wilson04@gmail.com>
2025-05-29 18:57:50 +0200

    refactor: streamline price extraction and gold checking logic, day done!

--- 48_day_browsergamebot_w_Selenium/cookieclicker.py
+++ 48_day_browsergamebot_w_Selenium/cookieclicker.py
@@ -1,81 +1,70 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuvwxyz-, '
+
+def get_prices():
+    prices = driver.find_elements(By.CSS_SELECTOR, value="#store b")
+    items = []
+    for price in prices:
+        items.append(price.text)
+    shaped_items = []
+    for item in items:
+        temp = ""
+        for ch in item:
+            if ch not in SYMBOLS:
+                temp += ch
+        shaped_items.append(temp)
+    shaped_items = [s for s in shaped_items if s]
+    int_list = []
+    for s in shaped_items:
+        try:
+            int_list.append(int(s))
+        except ValueError:
+            pass
+    return int_list
+
+def check_gold(gold, int_list):
+    if gold > int_list[7]:
+        driver.find_element(By.ID, "buyTime machine").click()
+    elif gold > int_list[6]:
+        driver.find_element(By.ID, "buyPortal").click()
+    elif gold > int_list[5]:
+        driver.find_element(By.ID, "buyAlchemy lab").click()
+    elif gold > int_list[4]:
+        driver.find_element(By.ID, "buyShipment").click()
+    elif gold > int_list[3]:
+        driver.find_element(By.ID, "buyMine").click()
+    elif gold > int_list[2]:
+        driver.find_element(By.ID, "buyFactory").click()
+    elif gold > int_list[1]:
+        driver.find_element(By.ID, "buyGrandma").click()
+    elif gold > int_list[0]:
+        driver.find_element(By.ID, "buyCursor").click()
+cps = driver.find_element(By.ID, value="cps")
+print(cps.text)
+bot_is_on = True
 cookie = driver.find_element(By.ID, value="cookie")
-cookie.click()
-
-money = driver.find_element(By.CSS_SELECTOR, value="#money")
-#print(money.text)
-
-prices = driver.find_elements(By.CSS_SELECTOR, value="#store b")
-items = []
-for price in prices:
-    #print(price.text)
-    items.append(price.text)
-#print(items)
-
-SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuvwxyz-, ' 
-shaped_items = []
-for item in items:
-    temp = ""
-    for ch in item:
-        if ch not in SYMBOLS:
-            temp += ch
-
-    shaped_items.append(temp)
-#print(shaped_items)
-shaped_items.pop()
-#print(shaped_items)
-int_list = []
-for str in shaped_items:
-    int_list.append(int(str))
-#print(int_list)    
-
-print(int_list[0])
-print(items[0])
-
-
-
-
-
-
-
-    # item.strip("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuvwxyz-")
-    # shaped_items.append(item)
-# items = items.strip("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuvwxyz-")
-# print(shaped_items)
-# item1 = driver.find_element(By.XPATH, value='//*[@id="buyCursor"]/b/text()[2]')
-# print(item1.text)
-# item2 = driver.find_element(By.XPATH, value='')
-# item3 = driver.find_element(By.XPATH, value='')
-# item4 = driver.find_element(By.XPATH, value='')
-# item5 = driver.find_element(By.XPATH, value='')
-# item6 = driver.find_element(By.XPATH, value='')
-# item7 = driver.find_element(By.XPATH, value='')
-# item8 = driver.find_element(By.XPATH, value='')
-
-# store = {
-#     "item":"",
-#         "price":""
-# }
-#while True:
-
-# exList = ['JM = {12, 23, 34, 45, 56}', 'the quick brown fox', 'word ($(,))']
-
-
-# results = []
-# for element in exList:
-#     temp = ""
-#     for ch in element:
-#         if ch not in SYMBOLS:
-#             temp += ch
-
-#     results.append(temp)
-
-# print results
+next_check = time.time() + 5
+timeout = time.time() + 60*5  
+while bot_is_on:
+    test = 0
+    cookie.click()
+    if test == 5 or time.time() > timeout:
+        break
+    test = test - 1
+    if time.time() > next_check:
+        money_text = driver.find_element(By.ID, "money").text.replace(",", "")
+        if money_text.isdigit():
+            money = int(money_text)
+            int_list = get_prices()
+            if len(int_list) >= 8:
+                check_gold(money, int_list)
+        next_check = time.time() + 5
+print(driver.find_element(By.ID, value="cp").text)
